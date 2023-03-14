9ce32486eff183b76f7ae27c901333c2c0b6b8f9
joseedu1309 <106010047+joseedu1309@users.noreply.github.com>
2023-03-14 07:49:39 -0700

    se agrega un mensaje

--- ejemplo git/src/ejemplo/git/EjemploGit.java
+++ ejemplo git/src/ejemplo/git/EjemploGit.java
@@ -1,19 +1,12 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package ejemplo.git;
 
-/**
- *
- * @author 701-17
- */
+
 public class EjemploGit {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
+        System.out.println("hola");
         // TODO code application logic here
     }
     
